c22c65013294d0c04537aa8631e5f08225c927a7
Sahil Arora <sahilaroraji2002@gmail.com>
2024-01-25 23:52:48 +0530

    handling admin module

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import { TelegramService } from 'src/telegram/telegram.service';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const telegramService = app.get(TelegramService);
-  await telegramService.setWebhook('https://037e-49-36-144-167.ngrok-free.app/telegram/webhook');
+  await telegramService.setWebhook('https://tele-weather-bot-l5zj.onrender.com/telegram/webhook');
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
